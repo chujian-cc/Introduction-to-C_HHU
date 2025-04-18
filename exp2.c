ffc69fdf922d19cdc5f1654700d9d64cc4fd290c
channabasava chola <47296565+channu7@users.noreply.github.com>
2025-04-18 12:42:26 +0530

    exp2.c

---
+++ exp2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  char c1,c2,c3,c4,c5;
+  printf("choose 5 Characters :");
+  c1 = getchar();
+  c2 = getchar();
+  c3 = getchar();
+  c4 = getchar();
+  c5 = getchar();
+  printf("encoded characters :");
+  puthar(c1+4);
+  puthar(c2+4);
+  puthar(c3+4);
+  puthar(c4+4);
+  puthar(c5+4);
+  return 0;
+}
