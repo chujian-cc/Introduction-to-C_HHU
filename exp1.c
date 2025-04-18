6f3f5a7c390a3b3595b192c91480b56a05022d84
channabasava chola <47296565+channu7@users.noreply.github.com>
2025-04-18 12:16:52 +0530

    Create exp1.c
    Program 1

---
+++ exp1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char c1 ='H',c2 ='A', c3 ='P', c4 ='P', c5 ='Y';
+    c1=c1+4;
+    c2=c2+4;
+    c3=c3+4;
+    c4=c4+4;
+    c5=c5+4;
+    printf("pws: %c%c%c%c%c\n",c1,c2,c3,c4,c5);
+    return 0;
+}
